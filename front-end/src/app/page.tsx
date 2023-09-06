6ad65e0b0402feef334b98a151f4fa8cb2a96713
Victor Almeida de Freitas <victor.freitas08@hotmail.com>
2023-09-05 23:22:09 -0300

    adding login

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -4,22 +4,32 @@ import {
   CheckIcon,
   XMarkIcon,
 } from "@heroicons/react/24/outline";
+import axios from "axios";
 import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import "./page.styles.css";
 
 interface ITask {
   id: string;
-  nome: string;
+  name: string;
+}
+
+interface IUser {
+  _id: string;
+  name: string;
+  email: string;
+  tasks: Array<ITask>;
 }
 
 export default function Home() {
   const [tasks, setTasks] = useState<Array<ITask>>([]);
   const [task, setTask] = useState("");
+  const [user, setUser] = useState<IUser>();
+  const [email, setEmail] = useState("");
 
   const handleAddTask = () => {
     if (!task.trim()) return;
-    setTasks([...tasks, { id: uuidv4(), nome: task }]);
+    setTasks([...tasks, { id: uuidv4(), name: task }]);
     setTask("");
   };
 
@@ -27,54 +37,94 @@ export default function Home() {
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
-  return (
-    <main className="flex min-h-screen flex-col justify-between items-center p-24">
-      <div className="container">
-        <p className="italic font-sans font-light text-3xl">
-          Cadastre uma tarefa
+  const handleLogin = async (): Promise<void> => {
+    if (!email.trim()) return;
+    const data = await axios.post("http://localhost:3000/auth/login ", {
+      email: email,
+    });
+    console.log(data.data.access_token);
+    const userData = await axios.get(`http://localhost:3000/users/${email}`, {
+      headers: { Authorization: `Bearer ${data.data.access_token}` },
+    });
+    setUser(userData.data);
+  };
+
+  const renderLogin = () => {
+    return (
+      <div className="flex min-h-screen flex-col justify-center items-center p-24">
+        <p className="italic font-sans font-light text-3xl mb-10">
+          Realize seu login
         </p>
         <div className="input-div px-2">
           <input
-            onChange={(e) => setTask(e.target.value)}
-            value={task}
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
             className="bg-white border-none border-transparent w-full text-black rounded-md outline-none"
           />
           <button
-            className=" text-white rounded-md h-8 w-8"
-            onClick={handleAddTask}
+            className="text-white rounded-md h-8 w-8"
+            onClick={handleLogin}
           >
             <ArrowRightIcon className="h-full w-full text-slate-400" />
           </button>
         </div>
-        <div className="tarefas-container">
-          {tasks.map((tarefa) => (
-            <div
-              className="flex flex-row border-b w-full justify-between items-center"
-              key={tarefa.id}
-            >
-              <p className="font-sans font-light text-xl">{tarefa.nome}</p>
-              <div className="flex flex-row w-14 justify-between">
-                <button
-                  className="rounded-md text-white h-6 w-6"
-                  onClick={handleRemoveTask.bind(null, tarefa.id)}
-                >
-                  <CheckIcon className="h-full w-full text-slate-400" />
-                </button>
-                <button
-                  className="rounded-md text-white h-6 w-6"
-                  onClick={handleRemoveTask.bind(null, tarefa.id)}
-                >
-                  <XMarkIcon className="h-full w-full text-slate-400" />
-                </button>
-              </div>
-            </div>
-          ))}
-        </div>
       </div>
-      {/* className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[300px] after:translate-x-3/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]" */}
-      <footer className="italic font-sans font-light text-lg">
-        Made by bigas
-      </footer>
-    </main>
+    );
+  };
+
+  return (
+    <>
+      {!user ? (
+        renderLogin()
+      ) : (
+        <main className="flex min-h-screen flex-col justify-between items-center p-24">
+          <div className="container">
+            <p className="italic font-sans font-light text-3xl">
+              Cadastre uma tarefa
+            </p>
+            <div className="input-div px-2">
+              <input
+                onChange={(e) => setTask(e.target.value)}
+                value={task}
+                className="bg-white border-none border-transparent w-full text-black rounded-md outline-none"
+              />
+              <button
+                className="text-white rounded-md h-8 w-8"
+                onClick={handleAddTask}
+              >
+                <ArrowRightIcon className="h-full w-full text-slate-400" />
+              </button>
+            </div>
+            <div className="tarefas-container">
+              {tasks.map((tarefa) => (
+                <div
+                  className="flex flex-row border-b w-full justify-between items-center"
+                  key={tarefa.id}
+                >
+                  <p className="font-sans font-light text-xl">{tarefa.name}</p>
+                  <div className="flex flex-row w-14 justify-between">
+                    <button
+                      className="rounded-md text-white h-6 w-6"
+                      onClick={handleRemoveTask.bind(null, tarefa.id)}
+                    >
+                      <CheckIcon className="h-full w-full text-slate-400" />
+                    </button>
+                    <button
+                      className="rounded-md text-white h-6 w-6"
+                      onClick={handleRemoveTask.bind(null, tarefa.id)}
+                    >
+                      <XMarkIcon className="h-full w-full text-slate-400" />
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+          <footer className="italic font-sans font-light text-lg">
+            Made by bigas
+          </footer>
+        </main>
+      )}
+    </>
   );
 }
